3a1a1df1fe493a70b316b14db0301cd8bbda674e
msdx321 <msdx321@gmail.com>
2018-01-03 14:35:27 +0800

    walleye: Bump version

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export SUBVER="R4"
+export SUBVER="R5"
 export LOCALVERSION="-mKernel-$SUBVER"
 
 ROOT_DIR=$(pwd)
--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export SUBVER="R3"
+export SUBVER="Nightly"
 export LOCALVERSION="-mKernel-$SUBVER"
 
 ROOT_DIR=$(pwd)
